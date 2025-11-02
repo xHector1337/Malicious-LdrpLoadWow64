cd2e72a1efd5151f8d7d8319b5cf3cbfe5383844
Muzaffer Umut ŞAHİN <140441119+xHector1337@users.noreply.github.com>
2025-11-02 14:33:16 +0300

    Fix RtlInitUnicodeString function pointer type
    Changed the function pointer type for RtlInitUnicodeString from NTSTATUS to VOID.

--- LdrpLoadWow64/lab.c
+++ LdrpLoadWow64/lab.c
@@ -4,7 +4,7 @@
 
 NTSTATUS LdrpLoadWow64(PWCHAR DllPath) {
 	HMODULE hNtdll = NULL;
-	NTSTATUS(NTAPI * RtlInitUnicodeString)(OUT PUNICODE_STRING DestinationString, IN OPTIONAL __drv_aliasesMem PWSTR SourceString) = NULL;
+	VOID(NTAPI * RtlInitUnicodeString)(OUT PUNICODE_STRING DestinationString, IN OPTIONAL __drv_aliasesMem PWSTR SourceString) = NULL;
 	NTSTATUS(NTAPI * LdrpLoadWow64)(PUNICODE_STRING DllPath) = NULL;
 	UNICODE_STRING unicodeDllPath = { 0 };
 	NTSTATUS retVal = STATUS_DLL_INIT_FAILED;
@@ -13,17 +13,14 @@ NTSTATUS LdrpLoadWow64(PWCHAR DllPath) {
 	if (hNtdll == NULL) {
 		error("GetModuleHandleA");
 	}
-	RtlInitUnicodeString = (NTSTATUS(NTAPI*)(PUNICODE_STRING, __drv_aliasesMem PWSTR))GetProcAddress(hNtdll, "RtlInitUnicodeString");
+	RtlInitUnicodeString = (VOID(*)(PUNICODE_STRING, __drv_aliasesMem PWSTR))GetProcAddress(hNtdll, "RtlInitUnicodeString");
 	if (RtlInitUnicodeString == NULL) {
 		error("GetProcAddress for RtlInitUnicodeString");
 	}
 	LdrpLoadWow64 = (NTSTATUS(NTAPI*)(PUNICODE_STRING))((CHAR*)hNtdll + LdrpLoadWow64Offset);
 
-	retVal = RtlInitUnicodeString(&unicodeDllPath, DllPath);
-	if (retVal) {
-		DbgPrint("[-][%s] %s returned 0x%x\n", __func__, "RtlInitUnicodeString", retVal);
-		goto exit;
-	}
+	RtlInitUnicodeString(&unicodeDllPath, DllPath);
+	
 	retVal = LdrpLoadWow64(&unicodeDllPath);
 
 exit:
